08b8abcc519d9e8990a2428d1a78232212926ad4
Konstantinos Sakkas <63365463+ksakkas@users.noreply.github.com>
2023-03-06 12:36:19 +0200

    Create Askisi2.m

---
+++ Codes/Lab 3/Askisi2.m
@@ -0,0 +1,17 @@
+I = imread('Noise_salt_and_pepper.png’);
+filtered = uint8(zeros(size(I,1),size(I,2))); 
+megethos_parathyrou = 3; 
+offset = (megethos_parathyrou/2) - 0.5;
+parathyro = zeros(megethos_parathyrou,megethos_parathyrou); 
+
+for i=1+offset:size(I,1)-offset   
+    for j=1+offset:size(I,2)-offset 
+        parathyro(:,:)=I(i-offset:i+offset,j-offset:j+offset); 
+        line_parathyro = parathyro(:);
+        median_parathyroy = median(line_parathyro);
+        filtered(i,j) = median_parathyroy;
+     end
+end
+
+figure, imshow(I) 
+figure, imshow(filtered)

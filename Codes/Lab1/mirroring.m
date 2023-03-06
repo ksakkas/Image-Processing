b783be118900ae03e39e7cc046edbc26ab08f1b8
Konstantinos Sakkas <63365463+ksakkas@users.noreply.github.com>
2023-03-06 09:06:15 +0200

    Create mirroring.m

---
+++ Codes/Lab1/mirroring.m
@@ -0,0 +1,14 @@
+function mirrored = mirroring(I)
+
+mirrored = zeros(size(I,1),size(I,2));
+count = 1;
+for i = size(I,2):-1:1
+    mirrored(:,count) = I(:,i);
+    count = count + 1;
+end
+
+
+%-----------------------
+I = imread('cameraman.tif');
+m = mirroring(I);
+figure, imshow(uint8(m))
